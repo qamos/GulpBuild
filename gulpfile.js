34924f90369932654429b109bc7b63b9717e675b
Oleg Zyuzin <qamos@bk.ru>
2022-01-23 04:21:42 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const sass = require('gulp-sass')(require('sass'))
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
 const concat = require('gulp-concat')
+const sourcemaps = require('gulp-sourcemaps')
+const imagemin = require('gulp-imagemin')
+const autoprefixer = require('gulp-autoprefixer')
 
 const paths = {
     styles: {
@@ -23,6 +26,10 @@ const paths = {
     img: {
         src: 'src/img/**/*',
         dest: 'dist/img/'
+    },
+    html: {
+        src: 'src/*.html',
+        dest: 'dist'
     }
 }
 
@@ -32,41 +39,52 @@ function clean() {
 
 function styles() {
     return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'expanded'
         }))
+        .pipe(autoprefixer({
+			cascade: false
+		}))
         .pipe(cleancss({
-            level: {
-                 1: {
-                      specialComments: 0 
-                    } 
-                } 
+            level: 2
             }))
         .pipe(rename({ 
             basename: 'main',
             suffix: '.min', prefix : '' 
         }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(paths.styles.dest))
 }
 
 function scripts() {
-    return gulp.src(paths.scripts.src, {
-        sourcemaps: true
-    })
-    .pipe(babel())
+    return gulp.src(paths.scripts.src)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
     .pipe(uglify())
     .pipe(concat('main.min.js'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.scripts.dest))
 }
 
 function img() {
     return gulp.src(paths.img.src)
+    .pipe(imagemin({
+        progressive: true,
+    }))
     .pipe(gulp.dest(paths.img.dest))
 }
 
 function fonts() {
     return gulp.src(paths.fonts.src)
     .pipe(gulp.dest(paths.fonts.dest))
+}
+
+function html() {
+    return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest))
 }
 
 function watch() {
@@ -76,9 +94,11 @@ function watch() {
     gulp.watch(paths.fonts.src, fonts)
 }
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts, fonts, img), watch)
+const build = gulp.series(clean, html, gulp.parallel(styles, scripts, fonts, img), watch)
 
 exports.clean = clean
+exports.img = img
+exports.html = html
 exports.styles = styles
 exports.scripts = scripts
 exports.watch = watch
